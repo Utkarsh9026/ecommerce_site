7f291ae3463f402e74a642336f56874d49748f8b
Utkarsh9026 <utkarshshahi9935@gamil.com>
2024-02-23 01:14:35 +0530

    update env method

--- admin/src/Components/ListProduct/ListProduct.jsx
+++ admin/src/Components/ListProduct/ListProduct.jsx
@@ -6,7 +6,7 @@ const ListProduct = () => {
   const [allproducts, setAllProducts] = useState([]);
 
   const fetchInfo = async () => {
-    await fetch("http://localhost:4000/allproducts")
+    await fetch(`${import.meta.env.VITE_SERVER}/allproducts`)
       .then((res) => res.json())
       .then((data) => {
         setAllProducts(data);
@@ -18,7 +18,7 @@ const ListProduct = () => {
   }, []);
 
   const remove_product = async (id) => {
-    await fetch("http://localhost:4000/removeproduct", {
+    await fetch(`${import.meta.VITE_SERVER}/removeproduct`, {
       method: "POST",
       headers: {
         Accept: "application/json",
