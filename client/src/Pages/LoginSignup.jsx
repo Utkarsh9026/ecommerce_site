545ae635efebbc911aaa2e041f3a1adc80cff655
Utkarsh9026 <utkarshshahi9935@gamil.com>
2024-02-23 23:35:03 +0530

    netlify update

--- client/src/Pages/LoginSignup.jsx
+++ client/src/Pages/LoginSignup.jsx
@@ -16,7 +16,7 @@ const LoginSignUp = () => {
   const login = async () => {
     console.log("login function executed", formData);
     let responseData;
-    await fetch("http://localhost:4000/login  ", {
+    await fetch(`${process.env.REACT_APP_SERVER}/login`, {
       method: "POST",
       headers: {
         Accept: "application/form-data",
@@ -38,7 +38,7 @@ const LoginSignUp = () => {
   const signup = async () => {
     console.log("signup function executed", formData);
     let responseData;
-    await fetch("http://localhost:4000/signup", {
+    await fetch(`${process.env.REACT_APP_SERVER}/signup`, {
       method: "POST",
       headers: {
         Accept: "application/form-data",
@@ -84,7 +84,7 @@ const LoginSignUp = () => {
             name="password"
             value={formData.password}
             onChange={changeHandler}
-            type="text"
+            type="password"
             placeholder="Password"
           />
         </div>
